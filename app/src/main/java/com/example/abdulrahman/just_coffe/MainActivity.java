6496fe29d98888ba2b81c52820faf267a9430e9f
abdulrahman-abdullah <nfs056@gmail.com>
2017-11-24 05:37:15 +0300

    create couple things

--- app/src/main/java/com/example/abdulrahman/just_coffe/MainActivity.java
+++ app/src/main/java/com/example/abdulrahman/just_coffe/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.abdulrahman.just_coffe;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,17 +10,16 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     TextView customerName , getFlavor,totalPrice,totalCups , counterCups;
     EditText etName ;
+
     RelativeLayout showInfo ;
     CheckBox boxOne ,boxTwo ;
-    boolean hasWippedCream ;
     // final int price for each cups .
-    final int price = 5 ;
-    int quantityCups ;
+     int price = 5 ;
     int orderCounter ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     *
+     * @param cups pass {@link #orderCounter}
+     */
     private void showQuantity(int cups){
        counterCups.setText(String.valueOf(cups));
     }
@@ -51,35 +57,84 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        setRelativeVisble();
         // set Customer name .
         customerName.setText(getCustomerName());
         // set flavor
-        getFlavor.setText(String.valueOf(checkStatusBox()));
+        StringBuilder str = new StringBuilder();
+        if (boxOne.isChecked())
+            str.append("Wipped cream, ");
+        if (boxTwo.isChecked())
+            str.append("And Chocolate ");
+        getFlavor.setText(str.toString());
         // set quantity cups .
        totalCups.setText(String.valueOf(getOrderCounter()));
        // set total Price .
-        totalPrice.setText(String.valueOf(getPrice()));
+        int priceWithFlavor = incrementPrice(hasWippedCreams(),isHasChocolate());
+        totalPrice.setText("$"+String.valueOf(priceWithFlavor));
+        String result = "name : " + getCustomerName() +"\n" + "Flavor : " + str+"\n" + " total price : $" + priceWithFlavor;
+        String subject = " order summary ";
+        openMailApp(subject,result);
     }
 
+    /**
+     * @return string  form edit text .
+     */
     public String getCustomerName(){
     return  etName.getText().toString();
     }
-    private void setRelativeVisble(){
-        etName.setVisibility(View.INVISIBLE);
-        showInfo.setVisibility(View.VISIBLE);
-    }
 
-    public boolean checkStatusBox(){
+    /**
+     * @return if customer check wiped return true otherwise return false .
+     */
+    public boolean hasWippedCreams(){
         return boxOne.isChecked();
     }
-    // get method price and quantity cups .
+
+    /**
+     * @return if customers check chocolate return true otherwise return false .
+     */
+    public boolean isHasChocolate(){
+        return boxTwo.isChecked();
+    }
+    /**
+     * @param check if customer choice wiped cream  flavor, add 10 $ price .
+     * @param  checkChocolate  if customers want chocolate .
+     * @return total price .
+     */
+    public int incrementPrice(boolean check,boolean checkChocolate){
+        int t = price * orderCounter ;
+        if (check && !checkChocolate ) {
+            return t + (orderCounter * 10); // 2 cups = 10 + 20 = 30 .
+        }
+        else if (check && checkChocolate) {
+            return t + (orderCounter * 10) + (orderCounter * 5); // 2 cups = 2* 5 , price = 10 + 20 for cream + 10 for chocolate. total = 40 .
+        }
+        if( !check && checkChocolate){
+            return t + 5 ;}
+        else {
+            return t;
+        }
+
+    }
+
+    /**
+     * @return how many cups the customer well take it .
+     */
     private int getOrderCounter(){
         return orderCounter;
     }
-    // get price .
-    private int getPrice(){
 
-        return getOrderCounter() * price ;
+    /**
+     * @param subject title of mail.
+     * @param message  content order of coffe .
+     */
+    public void openMailApp(String subject,String message ){
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+       intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+       intent.putExtra(Intent.EXTRA_TEXT,message);
+       if (intent.resolveActivity(getPackageManager()) !=null){
+           startActivity(intent);
+       }
     }
 }
